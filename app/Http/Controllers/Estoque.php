bed7e6b3b431d248b1c1327ed4ee46c2f4a01265
Igor Marines <igor.marines25@gmail.com>
2024-08-28 21:59:45 -0300

    feature: :sparkles: controller, model, migration, routes

---
+++ app/Http/Controllers/Estoque.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Estoque extends Controller
+{
+    public function listarEstoque()
+    {
+        $estoques = Estoque::all();
+        return response()->json($estoques);
+    }
+
+    public function criarEstoque(Request $request)
+    {
+        $nome = $request->input('nome');
+        $quantidade = $request->input('quantidade');
+        $categoria = $request->input('categoria');
+
+        $estoque = new Estoque();
+        $estoque->nome = $nome;
+        $estoque->quantidade = $quantidade;
+        $estoque->categoria = $categoria;
+        $estoque->save();
+    }
+
+    public function atualizarQuantidadeEstoque(Request $request)
+    {
+        $id = $request->route('id');
+        $action = $request->route('action');
+        $estoque = Estoque::find($id);
+
+        if ($action == 'adicionar') {
+            $estoque->quantidade += 1;
+        } else {
+            $estoque->quantidade -= 1;
+        }
+
+        $estoque->save();
+    }
+
+    public function deletarEstoque($id)
+    {
+        $estoque = Estoque::find($id);
+        $estoque->delete();
+    }
+
+}
