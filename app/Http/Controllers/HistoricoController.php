0f3dcaa2444a2e8bddd6ccea0ab4ab8d08230cc6
Igor Marines <igor.marines25@gmail.com>
2024-09-01 13:18:15 -0300

    chore: :wrench: Movido logica de criação para um service e novo metodo para listar todo o historico dos itens

--- app/Http/Controllers/HistoricoController.php
+++ app/Http/Controllers/HistoricoController.php
@@ -4,34 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Historico;
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class HistoricoController extends Controller
 {
-    public function listarHistorico()
+    // show all history
+    public function getAllHistory()
     {
         $historico = Historico::all();
-
         return response()->json([
             'historico' => $historico
-        ]);
-    }
-
-    public function criarHistorico(Request $request)
-    {
-        $historico = new Historico();
-
-        $userLogged = User::find(FacadesAuth::id())->name;
-        $historico->estoque_id = $request->estoque_id;
-        $historico->acao = $request->acao;
-        $historico->quantidade = $request->quantidade;
-
-        $historico->save();
-
-        return response()->json([
-            'message' => 'O usuário ' . $userLogged . ' criou um novo histórico com a ação ' . $request->acao
         ]);
     }
 }
